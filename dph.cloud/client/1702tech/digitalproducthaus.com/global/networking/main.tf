912c89941f74545553272af45c41543c8a4950bd
Bongani Masuku <bongani@1702tech.com>
2023-02-11 18:20:25 +0200

    rename networking folder

--- dph.cloud/client/1702tech/digitalproducthaus.com/global/networking/main.tf
+++ dph.cloud/client/1702tech/digitalproducthaus.com/global/networking/main.tf
@@ -1,5 +1,43 @@
 #####################################################
 #                                                   #
+#                        STATE                      #
+#                                                   #
+#####################################################
+
+terraform {
+  backend "s3" {
+    bucket = "dph-platform-terraform-remote-state"
+    key    = "client/1702tech/digitalproducthaus.com/global/networking/terraform.tfstate"
+    region = "eu-west-1"
+
+    dynamodb_table = "dph-platform-terraform-remote-state-locks"
+    encrypt        = true
+  }
+}
+
+#####################################################
+#                                                   #
+#                      VERSIONS                     #
+#                                                   #
+#####################################################
+
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      version = "4.8.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+#####################################################
+#                                                   #
 #                     VARIABLES                     #
 #                                                   #
 #####################################################
