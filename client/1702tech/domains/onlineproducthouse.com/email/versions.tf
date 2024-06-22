7a638757b3c531a5b59f8ed0ff5b7521ab3b0ebb
Bongani Masuku <bongani@1702tech.com>
2024-06-22 19:58:26 +0200

    DPH-1032 - email config for domain

---
+++ client/1702tech/domains/onlineproducthouse.com/email/versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.52.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.client_info.region
+
+  default_tags {
+    tags = {
+      project_name           = var.client_info.project_name
+      project_short_name     = var.client_info.project_short_name
+      service_name           = var.client_info.service_name
+      service_short_name     = var.client_info.service_short_name
+      environment_name       = var.client_info.environment_name
+      environment_short_name = var.client_info.environment_short_name
+    }
+  }
+}
