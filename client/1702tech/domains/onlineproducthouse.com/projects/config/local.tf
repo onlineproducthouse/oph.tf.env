1537d3bf1af4f260b6c275cfeeda172a36692384
Bongani Masuku <bongani@1702tech.com>
2024-06-23 00:13:15 +0200

    DPH-1032 - build runtime env vars config

---
+++ client/1702tech/domains/onlineproducthouse.com/projects/config/local.tf
@@ -0,0 +1,71 @@
+#####################################################
+#                                                   #
+#                   CONFIGURATION                   #
+#                                                   #
+#####################################################
+
+resource "random_uuid" "local_api_key_v1" {}
+resource "random_uuid" "local_htmltopdf_api_key_v1" {}
+
+locals {
+  local_env = {
+    api = {
+      protocol = "http"
+      host     = "127.0.0.1"
+      port     = data.terraform_remote_state.qa_api.outputs.qa.api.container.port
+    }
+
+    htmltopdf = {
+      protocol = "http"
+      host     = "127.0.0.1"
+      port     = data.terraform_remote_state.qa_api.outputs.qa.htmltopdf.container.port
+    }
+
+    www_app_url     = "http://127.0.0.1:3000"
+    portal_app_url  = "http://127.0.0.1:3001"
+    console_app_url = "http://127.0.0.1:3002"
+  }
+
+  local = [
+    { id = "local_env_name", path = local.paths.local, key = "ENVIRONMENT_NAME", value = "local" },
+    { id = "local_run_swagger", path = local.paths.local, key = "RUN_SWAGGER", value = "true" },
+    { id = "local_db_connection_string", path = local.paths.local, key = "DB_CONNECTION_STRING", value = "postgres://root:password@127.0.0.1:5432/localdb" },
+    { id = "local_redis_connection_string", path = local.paths.local, key = "REDIS_CONNECTION_STRING", value = "redis://127.0.0.1:6379" },
+    { id = "local_sg_api_key", path = local.paths.local, key = "SG_API_KEY", value = local.secrets.qa.sg_api_key },
+    { id = "local_fs_s3_bucket_name", path = local.paths.local, key = "FS_S3_BUCKET_NAME", value = data.terraform_remote_state.qa.outputs.qa.platform.file_service.bucket_name },
+
+    { id = "local_paystack_public_key", path = local.paths.local, key = "PAYSTACK_PUBLIC_KEY", value = local.secrets.qa.paystack.public_key },
+    { id = "local_paystack_secret_key", path = local.paths.local, key = "PAYSTACK_SECRET_KEY", value = local.secrets.qa.paystack.secret_key },
+
+    { id = "local_api_protocol", path = local.paths.local, key = "API_PROTOCOL", value = local.local_env.api.protocol },
+    { id = "local_api_host", path = local.paths.local, key = "API_HOST", value = local.local_env.api.host },
+    { id = "local_api_port", path = local.paths.local, key = "API_PORT", value = local.local_env.api.port },
+    { id = "local_api_keys", path = local.paths.local, key = "API_KEYS", value = join(",", [
+      random_uuid.local_api_key_v1.result,
+    ]) },
+
+    { id = "local_htmltopdf_protocol", path = local.paths.local, key = "HTMLTOPDF_PROTOCOL", value = local.local_env.htmltopdf.protocol },
+    { id = "local_htmltopdf_host", path = local.paths.local, key = "HTMLTOPDF_HOST", value = local.local_env.htmltopdf.host },
+    { id = "local_htmltopdf_port", path = local.paths.local, key = "HTMLTOPDF_PORT", value = local.local_env.htmltopdf.port },
+    { id = "local_htmltopdf_keys", path = local.paths.local, key = "HTMLTOPDF_KEYS", value = join(",", [
+      random_uuid.local_htmltopdf_api_key_v1.result,
+    ]) },
+
+    { id = "local_www_app_url", path = local.paths.local, key = "WWW_APP_URL", value = local.local_env.www_app_url },
+    { id = "local_portal_app_url", path = local.paths.local, key = "PORTAL_APP_URL", value = local.local_env.portal_app_url },
+    { id = "local_console_app_url", path = local.paths.local, key = "CONSOLE_APP_URL", value = local.local_env.console_app_url },
+
+    { id = "local_client_api_key", path = local.paths.local, key = "VUE_APP_LOCAL_CLIENT_API_KEY", value = random_uuid.local_api_key_v1.result },
+    { id = "local_client_api_protocol", path = local.paths.local, key = "VUE_APP_LOCAL_CLIENT_API_PROTOCOL", value = local.local_env.api.protocol },
+    { id = "local_client_ws_api_protocol", path = local.paths.local, key = "VUE_APP_LOCAL_CLIENT_WS_API_PROTOCOL", value = "ws" },
+    { id = "local_client_api_host", path = local.paths.local, key = "VUE_APP_LOCAL_CLIENT_API_HOST", value = local.local_env.api.host },
+    { id = "local_client_api_port", path = local.paths.local, key = "VUE_APP_LOCAL_CLIENT_API_PORT", value = local.local_env.api.port },
+    { id = "local_client_api_base_path", path = local.paths.local, key = "VUE_APP_LOCAL_CLIENT_API_BASE_PATH", value = "/api/v1" },
+  ]
+}
+
+#####################################################
+#                                                   #
+#                       OUTPUT                      #
+#                                                   #
+#####################################################
---
+++ client/1702tech/domains/onlineproducthouse.com/projects/config/qa.tf
@@ -0,0 +1,137 @@
+#####################################################
+#                                                   #
+#                   CONFIGURATION                   #
+#                                                   #
+#####################################################
+
+data "terraform_remote_state" "qa_cloud" {
+  backend = "s3"
+
+  config = {
+    bucket = "oph-cloud-terraform-remote-state"
+    key    = "client/1702tech/cloud/qa/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "qa_platform" {
+  backend = "s3"
+
+  config = {
+    bucket = "oph-cloud-terraform-remote-state"
+    key    = "client/1702tech/domains/onlineproducthouse.com/platform/qa/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "qa_api" {
+  backend = "s3"
+
+  config = {
+    bucket = "oph-cloud-terraform-remote-state"
+    key    = "client/1702tech/domains/onlineproducthouse.com/projects/api/qa/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "qa_www" {
+  backend = "s3"
+
+  config = {
+    bucket = "oph-cloud-terraform-remote-state"
+    key    = "client/1702tech/domains/onlineproducthouse.com/projects/www/qa/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+resource "random_uuid" "qa_api_key_v1" {}
+resource "random_uuid" "qa_htmltopdf_api_key_v1" {}
+
+locals {
+  qa_env = {
+    api = {
+      protocol = "https"
+      host     = data.terraform_remote_state.qa_api.outputs.qa.api.load_balancer.domain_name
+      port     = "${data.terraform_remote_state.qa_api.outputs.qa.api.container.port}"
+    }
+
+    htmltopdf = {
+      protocol = "https"
+      host     = data.terraform_remote_state.qa_api.outputs.qa.htmltopdf.load_balancer.domain_name
+      port     = "${data.terraform_remote_state.qa_api.outputs.qa.htmltopdf.container.port}"
+    }
+
+    www_app_url     = data.terraform_remote_state.qa_web.outputs.qa.www.host.id
+    portal_app_url  = data.terraform_remote_state.qa_web.outputs.qa.portal.host.id
+    console_app_url = data.terraform_remote_state.qa_web.outputs.qa.console.host.id
+
+    db_connection_string = join("", [
+      local.secrets.qa.db.protocol,
+      "://",
+      local.secrets.qa.db.username,
+      ":",
+      local.secrets.qa.db.password,
+      "@",
+      local.secrets.qa.db.host,
+      ":",
+      local.secrets.qa.db.port,
+      "/",
+      local.secrets.qa.db.name,
+    ])
+
+    redis_connection_string = join("", [
+      local.secrets.qa.redis.protocol,
+      "://",
+      local.secrets.qa.redis.username,
+      ":",
+      local.secrets.qa.redis.pwd,
+      "@",
+      local.secrets.qa.redis.host,
+      ":",
+      local.secrets.qa.redis.port,
+    ])
+  }
+
+  qa = [
+    { id = "qa_env_name", path = local.paths.qa, key = "ENVIRONMENT_NAME", value = "qa" },
+    { id = "qa_run_swagger", path = local.paths.qa, key = "RUN_SWAGGER", value = "true" },
+    { id = "qa_db_connection_string", path = local.paths.qa, key = "DB_CONNECTION_STRING", value = local.qa_env.db_connection_string },
+    { id = "qa_redis_connection_string", path = local.paths.qa, key = "REDIS_CONNECTION_STRING", value = local.qa_env.redis_connection_string },
+    { id = "qa_sg_api_key", path = local.paths.qa, key = "SG_API_KEY", value = local.secrets.qa.sg_api_key },
+    { id = "qa_fs_s3_bucket_name", path = local.paths.qa, key = "FS_S3_BUCKET_NAME", value = data.terraform_remote_state.qa.outputs.qa.platform.file_service.bucket_name },
+
+    { id = "qa_paystack_public_key", path = local.paths.qa, key = "PAYSTACK_PUBLIC_KEY", value = local.secrets.qa.paystack.public_key },
+    { id = "qa_paystack_secret_key", path = local.paths.qa, key = "PAYSTACK_SECRET_KEY", value = local.secrets.qa.paystack.secret_key },
+
+    { id = "qa_api_protocol", path = local.paths.qa, key = "API_PROTOCOL", value = local.qa_env.api.protocol },
+    { id = "qa_api_host", path = local.paths.qa, key = "API_HOST", value = local.qa_env.api.host },
+    { id = "qa_api_port", path = local.paths.qa, key = "API_PORT", value = local.qa_env.api.port },
+    { id = "qa_api_keys", path = local.paths.qa, key = "API_KEYS", value = join(",", [
+      random_uuid.qa_api_key_v1.result,
+    ]) },
+
+    { id = "qa_htmltopdf_protocol", path = local.paths.qa, key = "HTMLTOPDF_PROTOCOL", value = local.qa_env.htmltopdf.protocol },
+    { id = "qa_htmltopdf_host", path = local.paths.qa, key = "HTMLTOPDF_HOST", value = local.qa_env.htmltopdf.host },
+    { id = "qa_htmltopdf_port", path = local.paths.qa, key = "HTMLTOPDF_PORT", value = local.qa_env.htmltopdf.port },
+    { id = "qa_htmltopdf_keys", path = local.paths.qa, key = "HTMLTOPDF_KEYS", value = join(",", [
+      random_uuid.qa_htmltopdf_api_key_v1.result,
+    ]) },
+
+    { id = "qa_www_app_url", path = local.paths.qa, key = "WWW_APP_URL", value = local.qa_env.www_app_url },
+    { id = "qa_portal_app_url", path = local.paths.qa, key = "PORTAL_APP_URL", value = local.qa_env.portal_app_url },
+    { id = "qa_console_app_url", path = local.paths.qa, key = "CONSOLE_APP_URL", value = local.qa_env.console_app_url },
+
+    { id = "qa_client_api_key", path = local.paths.qa, key = "VUE_APP_LOCAL_CLIENT_API_KEY", value = random_uuid.qa_api_key_v1.result },
+    { id = "qa_client_api_protocol", path = local.paths.qa, key = "VUE_APP_LOCAL_CLIENT_API_PROTOCOL", value = local.qa_env.api.protocol },
+    { id = "qa_client_ws_api_protocol", path = local.paths.qa, key = "VUE_APP_LOCAL_CLIENT_WS_API_PROTOCOL", value = "ws" },
+    { id = "qa_client_api_host", path = local.paths.qa, key = "VUE_APP_LOCAL_CLIENT_API_HOST", value = local.qa_env.api.host },
+    { id = "qa_client_api_port", path = local.paths.qa, key = "VUE_APP_LOCAL_CLIENT_API_PORT", value = local.qa_env.api.port },
+    { id = "qa_client_api_base_path", path = local.paths.qa, key = "VUE_APP_LOCAL_CLIENT_API_BASE_PATH", value = "/api/v1" },
+  ]
+}
+
+#####################################################
+#                                                   #
+#                       OUTPUT                      #
+#                                                   #
+#####################################################
