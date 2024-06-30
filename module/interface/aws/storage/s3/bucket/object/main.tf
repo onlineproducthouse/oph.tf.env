fc11ce653d11533847bc3543eb1e0673a4d7569a
Bongani Masuku <bongani@1702tech.com>
2024-06-30 19:19:40 +0200

    DPH-1032 - deploy dev tools scripts to 1702tech shared storage

--- module/interface/aws/storage/s3/bucket/object/main.tf
+++ module/interface/aws/storage/s3/bucket/object/main.tf
@@ -6,9 +6,10 @@
 
 variable "object" {
   type = object({
-    key         = string
-    bucket_id   = string
-    source_path = string
+    key            = string
+    bucket_id      = string
+    source_path    = string
+    content_base64 = string
   })
 }
 
@@ -21,9 +22,10 @@ variable "object" {
 resource "aws_s3_object" "object" {
   bucket                 = var.object.bucket_id
   key                    = var.object.key
-  source                 = var.object.source_path
+  source                 = var.object.source_path == null ? null : var.object.source_path
+  content_base64         = var.object.content_base64 == null ? null : var.object.content_base64
   server_side_encryption = "AES256"
-  etag                   = filemd5(var.object.source_path)
+  etag                   = var.object.source_path == null ? null : filemd5(var.object.source_path)
 }
 
 #####################################################
--- shared/developer_tools/scripts/main.tf
+++ shared/developer_tools/scripts/main.tf
@@ -69,6 +69,7 @@ output "scripts" {
   value = {
     for script in local.scripts : script.name => {
       key            = script.key,
+      name           = script.name,
       etag           = filemd5(script.source_path),
       content_base64 = filebase64(script.source_path),
     }
