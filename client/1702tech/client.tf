174a7d45c94fc18fcac4aee2714172182939959c
Bongani Masuku <bongani@1702tech.com>
2024-06-30 19:23:36 +0200

    DPH-1032 - complete trust relationship between client and OPH

--- client/1702tech/client.tf
+++ client/1702tech/client.tf
@@ -27,6 +27,27 @@ resource "aws_iam_role" "default" {
   managed_policy_arns = data.terraform_remote_state.iam_policies.outputs.policy.arn_list.operations
 }
 
+data "aws_iam_policy_document" "client_to_default" {
+  provider = aws.client
+  statement {
+    actions   = ["sts:AssumeRole"]
+    resources = [aws_iam_role.default.arn]
+  }
+}
+
+resource "aws_iam_policy" "client_to_default" {
+  provider = aws.client
+  name     = local.role_name.shared
+  path     = "/"
+  policy   = data.aws_iam_policy_document.client_to_default.json
+}
+
+resource "aws_iam_role_policy_attachment" "client_to_default" {
+  provider   = aws.client
+  role       = aws_iam_role.client.name
+  policy_arn = aws_iam_policy.client_to_default.arn
+}
+
 #####################################################
 #                                                   #
 #                       OUTPUT                      #
