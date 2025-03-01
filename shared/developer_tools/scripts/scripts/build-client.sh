a6d6c067644ae46110b7f9a5e984aefa2522583e
Bongani Masuku <bongani@1702tech.com>
2025-03-01 14:24:22 +0200

    OPH-101 - cleanup

--- shared/developer_tools/scripts/scripts/build-client.sh
+++ shared/developer_tools/scripts/scripts/build-client.sh
@@ -14,19 +14,10 @@ echo "Downloading $DEV_TOOLS_STORE_SCRIPTS$LOAD_ENV_VARS_SCRIPT"
 aws s3 cp $(echo "$DEV_TOOLS_STORE_SCRIPTS$LOAD_ENV_VARS_SCRIPT") $(echo "$CI_FOLDER$LOAD_ENV_VARS_SCRIPT")
 
 source $(echo "$CI_FOLDER$LOAD_ENV_VARS_SCRIPT") $AWS_REGION $AWS_SSM_PARAMETER_PATHS $(pwd)
+cp $ENV_FILE $(pwd)/packages/ui
 
 n "22.13.1" && npm i
 
-source $ENV_FILE && npm run build \
-  -w packages/config \
-  -w packages/constants \
-  -w packages/utilities \
-  -w packages/contracts \
-  -w packages/api \
-  -w packages/ui
-
-if [[ -n "$TARGET_WORKSPACE" ]]; then
-  source $ENV_FILE && npm run build -w $TARGET_WORKSPACE
-fi
+source $ENV_FILE && npm run build
 
 echo "Done."
