9416a17fdb14e94ac526f8317689a57fcee244e9
Bongani Masuku <bongani@1702tech.com>
2025-03-02 21:30:26 +0200

    OPH-111 - set environment name constants

---
+++ shared/environments/main.tf
@@ -0,0 +1,58 @@
+#####################################################
+#                                                   #
+#                        STATE                      #
+#                                                   #
+#####################################################
+
+terraform {
+  backend "s3" {
+    bucket = "oph-cloud-terraform-remote-state"
+    key    = "shared/environments/terraform.tfstate"
+    region = "eu-west-1"
+
+    dynamodb_table = "oph-cloud-terraform-remote-state-locks"
+    encrypt        = true
+  }
+}
+
+#####################################################
+#                                                   #
+#                     VARIABLES                     #
+#                                                   #
+#####################################################
+
+variable "client_info" {
+  type = object({
+    region = string
+
+    project_name       = string
+    project_short_name = string
+
+    service_name       = string
+    service_short_name = string
+
+    environment_name       = string
+    environment_short_name = string
+  })
+}
+
+#####################################################
+#                                                   #
+#                   CONFIGURATION                   #
+#                                                   #
+#####################################################
+
+#####################################################
+#                                                   #
+#                       OUTPUT                      #
+#                                                   #
+#####################################################
+
+output "environments" {
+  value = {
+    local = "local"
+    test  = "test"
+    qa    = "qa"
+    prod  = "prod"
+  }
+}
